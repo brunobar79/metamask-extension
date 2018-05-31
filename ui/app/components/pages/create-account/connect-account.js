d3a9bd0f0fa210e50dff25f3580872ed115ce2ea
Bruno <brunobar79@gmail.com>
2018-05-31 03:29:22 -0400

    showing ETH addresses correctly

--- ui/app/components/pages/create-account/connect-account.js
+++ ui/app/components/pages/create-account/connect-account.js
@@ -4,42 +4,108 @@ const h = require('react-hyperscript')
 const connect = require('react-redux').connect
 const actions = require('../../../actions')
 const TrezorConnect = require('../../../../lib/trezor-connect.js')
+const HDKey = require('hdkey')
+const { publicToAddress, toChecksumAddress } = require('ethereumjs-util')
 
 class ConnectAccountForm extends Component {
-    constructor(props, context) {
-        super(props)               
+    constructor (props, context) {
+        super(props)
         this.state = {
-            address: null,
-            error: null
+            error: null,
+            response: null,
+            wallets: [],
         }
 
-        console.log("TrezorConnect?", TrezorConnect)
+        console.log('TrezorConnect?', TrezorConnect)
     }
 
-    connectToTrezor(){
-        var path = "m/44'/60'/0'/0" 
+    getDeterministicWallets (data) {
+        const {publicKey, chainCode} = data
+        const hdk = new HDKey()
+        hdk.publicKey = new Buffer(publicKey, 'hex')
+        hdk.chainCode = new Buffer(chainCode, 'hex')
+        const pathBase = 'm'
 
-        TrezorConnect.ethereumGetAddress(path, response => {
-            console.log(response);
+        const wallets = []
+        for (let i = 0; i < 10; i++) {
+            const dkey = hdk.derive(`${pathBase}/${i}`)
+            const address = publicToAddress(dkey.publicKey, true).toString('hex')
+            wallets.push({
+                i,
+                address: toChecksumAddress(address),
+                balance: 0,
+                short_address: `${address.slice(
+                    0,
+                    10
+                )}...${address.slice(-10)}`,
+            })
+        }
+        return wallets
+    }
+
+    connectToTrezor () {
+        var path = "m/44'/60'/0'/0"
+
+        TrezorConnect.getXPubKey(path, response => {
+            console.log(response)
             if (response.success) {
-              this.setState({
-                address: `${response.address.slice(
-                  0,
-                  4
-                )}...${response.address.slice(-4)}`,
-                error: null
-              });
+
+            const wallets = this.getDeterministicWallets(response)
+
+
+                this.setState({
+                    response,
+                    error: null,
+                    address: `${response.publicKey.slice(
+                        0,
+                        12
+                    )}...${response.publicKey.slice(-12)}`,
+                    wallets,
+
+                })
+
+
             } else {
-              this.setState({ error: response.error, address: null });
-              console.error("Error:", response.error); // error message
+              this.setState({ error: response.error, address: null })
+              console.error('Error:', response.error) // error message
             }
-          }, "1.4.0");
+          }, '1.4.0')
           // 1.4.0 is first firmware that supports ethereum
+    }
+
+    renderWallets () {
+        if(!this.state.wallets.length) return null;
+        return [
+             h(
+            "div.new-account-create-form__input-label",
+            {
+                style: {
+                    margin: 12,
+                    textAlign: "center",
+                    fontWeight: 'bold'
+                },
+                key: 'title',
+            },
+                ['Your trezor device contains the following ETH addresses:']
+            ),
+            ...this.state.wallets.map((w, i) => {
+            return h(
+              "div.new-account-create-form__input-label",
+              {
+                style: {
+                  margin: 12,
+                  textAlign: "center"
+                },
+                  key: `wallet-${i}`,
+              },
+              [`${w.short_address} - ${w.balance} ETH`, h("br")]
+            )
+        })]
     }
 
     render () {
        return h('div.new-account-create-form', [
-        
+
         h('button.btn-primary--lg', {
           onClick: () => {
             this.connectToTrezor()
@@ -47,18 +113,11 @@ class ConnectAccountForm extends Component {
             style: {
                 margin: 12,
             },
-        }, 
+        },
           'Connect to Trezor',
         ),
-           this.state.address ? h('div.new-account-create-form__input-label', {style: {
-               margin: 12,
-               textAlign: 'center',
-           }}, [
-            `Succesfully connected to address`,
-            h('br'),
-            h('b', this.state.address),
-           ]) : null,
-           this.state.error ? h('span.error', { style: {
+        this.renderWallets(),
+        this.state.error ? h('span.error', { style: {
                margin: 12,
            }}, this.state.error) : null,
         ])
